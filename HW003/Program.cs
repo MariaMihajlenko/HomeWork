3ef40f3323258f7b550007a4b2fddbe76015ee8e
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-08 20:20:11 +0500

    задача 4 наброски

---
+++ HW003/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("Четное");
+    Console.WriteLine(number);
+
+}
+else
+{
+    Console.WriteLine("Нечетное");
+    Console.WriteLine(number);
+
+}
---
+++ HW004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите любое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number;
+
+while(true)
+{
+if ((number % 2) == 0)
+
+
+{
+    Console.WriteLine("Четные числа ряда до N");
+    Console.WriteLine(number  number-1);
+
+}
+Count=Count-1;
+}
