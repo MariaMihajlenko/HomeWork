5865c19b64836fd3ccd0d79e5157f8655b7f14a5
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-08 18:58:48 +0500

    сравнение чисел

---
+++ HW001/Program.cs
@@ -0,0 +1,29 @@
+﻿    Console.WriteLine("Введите первое число ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+    if (number1 > number2)
+    {
+        Console.WriteLine("большее число");
+        Console.WriteLine(number1);
+        Console.WriteLine("меньшее число");
+        Console.WriteLine(number2);
+    }
+    else if (number1 <number2)
+    {
+        Console.WriteLine("большое число");
+        Console.WriteLine(number2);
+        Console.WriteLine("меньшее число");
+        Console.WriteLine(number1);
+
+    }
+    else if (number1 == number2)
+
+    {
+        Console.WriteLine("числа равны");
+      
+    }
+    
+    
